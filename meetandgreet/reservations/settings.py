2a7cd478f468259072c94bee02999470d79ac0ee
Vlad <dr.melix@gmail.com>
2024-06-07 13:35:04 +0300

    Добавил Swagger

--- meetandgreet/reservations/settings.py
+++ meetandgreet/reservations/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'drf_yasg',
     'mg',
     'accounts',
 ]
--- meetandgreet/reservations/urls.py
+++ meetandgreet/reservations/urls.py
@@ -2,13 +2,35 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework.urlpatterns import format_suffix_patterns
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="Description of your API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('service/', include("mg.urls")),
     path('api/', include("rest_framework.urls")),
     path('accounts/', include('accounts.urls')),
-
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Добавьте эту строку только в случае, если DEBUG=True в вашем файле settings.py
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
