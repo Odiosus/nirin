3065063477a1929c95f2ece4a94ed7f3c0ec09ba
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-06-13 11:13:27 +0300

    Удалены остатки прошлого React

--- meetandgreet/src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Title({title}) {
-    return (
-        <div className="title text-center my-5">
-            <h4>{title}</h4>
-        </div>
-    )
-}
