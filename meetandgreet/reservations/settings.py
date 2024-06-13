017d20c90252f41222d325f145efd1318dd00b4c
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-06-13 12:07:49 +0300

    Проверка, просмотр URL

--- meetandgreet/reservations/settings.py
+++ meetandgreet/reservations/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    # 'corsheaders',
     'rest_framework',
     'drf_yasg',
     'mg',
--- meetandgreet/reservations/urls.py
+++ meetandgreet/reservations/urls.py
@@ -5,28 +5,27 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from rest_framework.urlpatterns import format_suffix_patterns
-
-...
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Your API",
-      default_version='v1',
-      description="Description of your API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@yourapi.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Description of your API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('service/', include("mg.urls")),
-    path('api/', include("rest_framework.urls")),
-    path('accounts/', include('accounts.urls')),
+    # http://127.0.0.1:8000/api-auth/login/ - вводим логин/пароль суперюзера (404-ok)
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include("mg.urls")),
+
+    # path('accounts/', include('accounts.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
