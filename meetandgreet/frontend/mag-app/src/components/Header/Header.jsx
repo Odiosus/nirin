8242c69eaee408e166743f122cef1e918fe37558
makedonzp <freefilosof@gmail.com>
2024-06-20 22:44:57 +0300

    sync

--- meetandgreet/frontend/mag-app/src/components/Header/Header.jsx
+++ meetandgreet/frontend/mag-app/src/components/Header/Header.jsx
@@ -4,17 +4,8 @@ import Video from "./video/Video";
 import Navigation from "./Navigation/Navigation";
 import Title from "./Title/Title";
 import Test from "./Test/Test";
-// import departure_icon from "../../assets/airplane-came.png";
-// import transit_icon from "../../assets/plane_away_and_came.png";
-// import arrival_icon from "../../assets/plane_arrived.png";
-// import arrow_btn_right from "../../assets/arrow_btn_services.png";
 
 export default function Header() {
-  // const width_main = `clamp(14.375rem, 13.25rem + 4.63vw, 20rem)`;
-  // const height_main = `clamp(4.813rem, 2.75rem + 1.88vw, 5rem)`;
-  // const height_right = `clamp(4.813rem, 2.563rem + 1.25vw, 4.063rem)`;
-  // const width_right = `clamp(3.125rem, 2.75rem + 0.88vw, 5rem)`;
-
   return (
     <>
       <div className={styles.header}>
