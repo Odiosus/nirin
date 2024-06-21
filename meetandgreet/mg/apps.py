d81f3ea50e09131d34e55ebe71f9b41bce345ddc
Владислав Меликсет <dr.melix@gmail.com>
2024-06-22 00:34:19 +0300

    Delete meetandgreet/mg directory

--- meetandgreet/mg/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServiceAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mg'
-    verbose_name = 'Услуги "Meet + Greet"'
