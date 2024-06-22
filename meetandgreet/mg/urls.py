f3777d7a6aa2d0822ee266d2bc3124b8acf4783c
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-06-22 12:55:23 +0300

    добавил урл Feedback

--- meetandgreet/mg/urls.py
+++ meetandgreet/mg/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('airport/', views.AirportListView.as_view()),  # доступ к списку всех аэропортов
     path('booking/', views.BookingCreateApiView.as_view()),  # доступ к форме создания брони
     path('airports/', SearchAirportViewSet.as_view({'get': 'list'})),  # доступ к списку всех аэропортов
-    path('fast_booking/', views.FastBookingCreateApiView.as_view())  # создание быстрого бронирования
+    path('fast_booking/', views.FastBookingCreateApiView.as_view()),  # создание быстрого бронирования
     path('feedback/', views.FeedbackCreateApiView.as_view())  # обратная связь
 ]
